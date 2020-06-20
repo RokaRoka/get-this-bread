1da88db01c2a079077a9237cc4cc161a799573ed
Sam Scott <samscott497@gmail.com>
2020-06-20 17:58:20 -0400

    Player teleporting

--- Player.gd
+++ Player.gd
@@ -4,9 +4,17 @@ const SPEED = 60
 
 var velocity = Vector2()
 
+export (float) var teleportDist = 60.0
+var teleporting = false
 var lastDir = "Down"
 
+func _ready():
+	$Teleporter.connect("teleport_finished", self, "teleportDone")
+
 func _physics_process(_delta: float) -> void:
+	if teleporting:
+		return
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
@@ -22,7 +30,8 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity.y = 0
 	
-	
+	if Input.is_action_just_pressed("ui_accept"):
+		startTeleport()
 	
 	
 	if velocity.x > 0:
@@ -57,3 +66,28 @@ func _physics_process(_delta: float) -> void:
 	
 	
 	velocity = move_and_slide(velocity)
+
+func startTeleport():
+	teleporting = true
+	var dir = Vector2()
+	match(lastDir):
+		"Up":
+			dir = Vector2.UP
+		"UR":
+			dir = Vector2.UP + Vector2.RIGHT
+		"Right":
+			dir = Vector2.RIGHT
+		"DR":
+			dir = Vector2.RIGHT + Vector2.DOWN
+		"Down":
+			dir = Vector2.DOWN
+		"DL":
+			dir = Vector2.LEFT + Vector2.DOWN
+		"Left":
+			dir = Vector2.LEFT
+		"UL":
+			dir = Vector2.UP + Vector2.LEFT
+	$Teleporter.goto(position + dir * teleportDist)
+
+func teleportDone():
+	teleporting = false
