5b0894066a7f491a33d39fb83e6bd836ad0c8ad8
Diyab Khan <dklpftw@gmail.com>
2020-06-20 17:17:30 -0400

    Mostly Tile Stuff

--- Player.gd
+++ Player.gd
@@ -4,36 +4,56 @@ const SPEED = 60
 
 var velocity = Vector2()
 
-
+var lastDir = "Down"
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
-		$AnimationPlayer.play("walkRight")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
-		$AnimationPlayer.play("walkLeft")
 	else:
 		velocity.x = 0
-		if Input.is_action_just_released("ui_right"):
-			$AnimationPlayer.play("idleRight")
-		if Input.is_action_just_released("ui_left"):
-			$AnimationPlayer.play("idleLeft")
 	
 	
 	if Input.is_action_pressed("ui_up"):
 		velocity.y = -SPEED
-		$AnimationPlayer.play("walkUp")
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y = SPEED
-		$AnimationPlayer.play("walkDown")
 	else:
 		velocity.y = 0
-		if Input.is_action_just_released("ui_up"):
-			$AnimationPlayer.play("idleUp")
-		if Input.is_action_just_released("ui_down"):
-			$AnimationPlayer.play("idleDown")
 	
+	
+	
+	
+	if velocity.x > 0:
+		if velocity.y > 0:
+			$AnimationPlayer.play("walkDiagDR")
+			lastDir = "DR"
+		elif velocity.y < 0:
+			$AnimationPlayer.play("walkDiagUR")
+			lastDir = "UR"
+		else:
+			$AnimationPlayer.play("walkRight")
+			lastDir = "Right"
+	elif velocity.x < 0:
+		if velocity.y > 0:
+			$AnimationPlayer.play("walkDiagDL")
+			lastDir = "DL"
+		elif velocity.y < 0:
+			$AnimationPlayer.play("walkDiagUL")
+			lastDir = "UL"
+		else:
+			$AnimationPlayer.play("walkLeft")
+			lastDir = "Left"
+	else:
+		if velocity.y < 0:
+			$AnimationPlayer.play("walkUp")
+			lastDir = "Up"
+		elif velocity.y > 0:
+			$AnimationPlayer.play("walkDown")
+			lastDir = "Down"
+		else: # both x and y are 0
+			$AnimationPlayer.stop(true)
 	
 	
 	velocity = move_and_slide(velocity)
